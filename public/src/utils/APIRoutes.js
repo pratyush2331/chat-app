ebf4b919c3be8c1ac3dc1d8a7f4f91ed09b8e92f
Pratyush Raj <pratyush2331@gmail.com>
2023-10-08 23:37:55 +0530

    Deployment changes rev2

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://chat-app-dun-ten.vercel.app";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
